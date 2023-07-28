f23b7eb15052e7645509a2396058eef2a01c6979
This-is-leeo <135179953+This-is-leeo@users.noreply.github.com>
2023-07-27 20:42:48 -0700

    Added distance based Waypoint

--- ROAR/agent_module/special_agents/segment_waypoint_generating_agent.py
+++ ROAR/agent_module/special_agents/segment_waypoint_generating_agent.py
@@ -13,29 +13,29 @@ class WaypointGeneratingAgent(Agent):
         self.output_file_path: Path = self.output_folder_path / "easy_map_waypoints.txt"
         self.waypoints_list = []
         print(f"Writing to array: {self.vehicle.transform}")
-        self.waypoints_list.append(self.vehicle.transform.record() + '\n')
-
-    def get_distance(last_location, current_location):
-        #returns distance between last location and current location
-        last_coordinates = last_location.split(',')[:3]
-        x1, z1, y1 = map(float, last_coordinates)
-        current_coordinates = last_location.split(',')[:3]
-        x2, z2, y2 = map(float, current_coordinates)
-        # distance = self.vehicle.transform.location.distance(self.last_waypoint.location)
-        distance = math.sqrt((x1-x2)**2+(z1-z2)**2+(y1-y2)**2) #distance formula of two point
-        return distance
+        self.waypoints_list.append(self.vehicle.transform.record() + "\n")   
+        self.last_waypoint = self.vehicle.transform
 
     def run_step(self, sensors_data: SensorsData,
                  vehicle: Vehicle) -> VehicleControl:
         super(WaypointGeneratingAgent, self).run_step(sensors_data=sensors_data,
                                                      vehicle=vehicle)
-        i = 1000
-
         # print(str)
-        # print(self.waypoints_list[-1])         
-        if self.get_distance(str(self.waypoints_list[-1]), str(self.vehicle.transform.record())) > 1:
-            print(f"Writing to array: {self.vehicle.transform}")
-            self.waypoints_list.append(self.vehicle.transform.record() + '\n')
+        # print(self.waypoints_list[-1])
+        # #self.get_distance(str(self.waypoints_list[-1]), str(self.vehicle.transform.record())) > 1:         
+        if self.time_counter > 1:
+            #print(f"Writing to [{self.output_file_path}]: {self.vehicle.transform}")
+            if self.last_waypoint == None:
+                self.last_waypoint = self.vehicle.transform
+                if self.vehicle.transform.location.x != 0:
+                    self.waypoints_list.append(self.vehicle.transform.record() + "\n")
+                print('run_step')
+            else:
+                distance = self.vehicle.transform.location.distance(self.last_waypoint.location)
+                if distance >= 1:
+                    self.waypoints_list.append(self.vehicle.transform.record() + "\n")
+                    self.last_waypoint = self.vehicle.transform
+                    print(f"Writing to array: {self.vehicle.transform}")
         return VehicleControl()
     
 
