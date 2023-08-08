af5ae1266feafdab2d7a2d49300de82bf6c324a7
Eric <lderic0415@gmail.com>
2023-08-07 18:36:16 -0700

    delete sectional pid files

--- test_code_delete_later.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from ROAR.control_module.section_pid_controller import SectionalPID
-
-
-test_file_path = Path("./ROAR/datasets/control/sectional_pid/test")
-with open(test_file_path, 'r') as test_file:
-    test1 = SectionalPID(test_file)
-    print(test1.array)
