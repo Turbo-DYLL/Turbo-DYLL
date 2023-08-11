9c32d36b5c4de0cf69765414f2f93a9261ccdbbc
Eric Li <lderic0415@gmail.com>
2023-08-11 14:40:28 -0700

    temp commit

--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 import carla
-
+from ROAR.utilities_module.data_structures_models import Transform, Location, Rotation
 
 def convert_transform_from_str_to_list(str_transform: str):
     coordinates = str_transform.split(",")
@@ -42,3 +42,11 @@ def convert_transform_from_str_to_source(line):
     x, y, z, roll, pitch, yaw = convert_transform_from_str_to_list(line)
     return carla.Transform(convert_location_from_agent_to_source(x, y, z),
                            convert_rotation_from_agent_to_source(roll, pitch, yaw))
+
+
+def convert_transform_from_str_to_agent(line):
+    x, y, z, roll, pitch, yaw = convert_transform_from_str_to_list(line)
+    return Transform(
+        location=Location(x=x, y=y, z=z),
+        rotation=Rotation(roll=roll, pitch=pitch, yaw=yaw)
+    )
