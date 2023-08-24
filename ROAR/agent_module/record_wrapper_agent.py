bec2763681e0c92af84cab370e6a93e4bc2c5423
Eric Li <lderic0415@gmail.com>
2023-08-23 22:16:29 -0700

    fixes agent ends earlier than expectation

--- ROAR/agent_module/record_wrapper_agent.py
+++ ROAR/agent_module/record_wrapper_agent.py
@@ -11,17 +11,16 @@ class RecordWrapperAgent(Agent):
         self.waypoint_record_list = waypoint_record_list
         self.time_list = []
         self.carla_world = carla_runner.world.carla_world
-        if end_location is not None:
-            self.end_location = end_location
-        else:
-            self.end_location = waypoint_record_list[-1]
+        self.end_location = end_location
 
     def run_step(self, vehicle, sensors_data):
         super().run_step(vehicle=vehicle, sensors_data=sensors_data)
         if self.vehicle.transform.location.distance(self.waypoint_record_list[0]) < 10:
             self.waypoint_record_list.pop(0)
             self.time_list.append(self.carla_world.get_snapshot().elapsed_seconds)
-        if self.vehicle.transform.location.distance(self.end_location) < 10:
+        if self.end_location and self.vehicle.transform.location.distance(self.end_location) < 10:
+            raise Exception("End of path")
+        elif len(self.waypoint_record_list) == 0:
             raise Exception("End of path")
 
         return self.agent.run_step(vehicle, sensors_data)
